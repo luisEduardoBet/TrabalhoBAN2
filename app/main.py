be210437c11cd11a0fe4d126f7c0d0a56dbf8a79
luisEduardoBet <luiseduardobet22@outlook.com>
2025-07-03 16:34:30 -0300

    mongo

--- app/main.py
+++ app/main.py
@@ -1,92 +1,83 @@
-from flask import Flask, render_template, request, flash, redirect, url_for, session, g
+from flask import Flask, render_template, request, flash, redirect, url_for, session
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
-from sqlalchemy import text
-from models.database import db
-from models.models import Bibliotecario, Usuario
-import datetime
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from models.models import User
 
 
 app = Flask(__name__)
 app.secret_key = "123123"
 lg_manager = LoginManager(app)
 lg_manager.login_view = "login"
-app.app_context().push()
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost:5432/biblioteca'
-db.init_app(app)
-
+app.config['MONGO_URI'] = "mongodb://localhost:27017/biblioteca"
+mongo =  PyMongo(app)
 
 
 
 @lg_manager.user_loader
 def user_loader(id): 
-  tabela = session.get('table')
+  t_user = session.get('user_type')
 
-  if tabela == 1: 
-    bib = db.session.query(Bibliotecario).filter_by(id_bibliotecario=id).first()
-    return bib
-  
-  elif tabela == 2: 
-    user = db.session.query(Usuario).filter_by(id_usuario=id).first()
-    return user
-  else: 
-    return None
+  if t_user == 3: 
+    user =  mongo.db.usuario.find_one({"_id": ObjectId(id)})
+    return User(user); 
 
-  
+  elif t_user == 2: 
+    user =  mongo.db.assistente.find_one({"_id": ObjectId(id)})
+    return User(user); 
+
+  else:
+    user =  mongo.db.bibliotecario.find_one({"_id": ObjectId(id)})
+    return User(user); 
+
+
+
+
+
 
 
 @app.route("/home", methods = ["GET", "POST"])
 @login_required
 def home():
-
+  
   if request.method == "POST": 
+    pipeline = [
 
-    pesquisa = request.form["pesquisa"]
+      { "$lookup":{ 
+          "from": "livro",
+          "localField": "livro",
+          "foreignField" : "_id",
+          "as": "livro_exemplar"
+        }
+      },
 
-    
-    if (pesquisa == ''): 
-    
-      resultado = db.session.execute(
-        text('SELECT isbn, esta_emprestado, id_exemplar FROM livro join exemplar using(id_livro) WHERE eh_reserva = false')
-      ).fetchall()
-      
-      return render_template("home.html", resultado = resultado)
+      {"$unwind" : "$livro_exemplar"},
+
+      {"$project": {"_id": 1, "nome": "$livro_exemplar.nome", "ISBN": "$livro_exemplar.ISBN"}}
+    ]
+
+    teste = list(mongo.db.exemplar.aggregate(pipeline))
+  
+    return render_template("home.html", resultado = teste)
 
   return render_template("home.html")
 
-  
 @app.route("/efetivar_emprestimo", methods=["GET", "POST"])
 @login_required
 def emprestimo():
 
-  if request.method == "POST": 
-
-    exemplares = request.form.getlist('checkbox')
-    data = str(datetime.date.today())
-    id = current_user.get_id()
-    for exemplar in exemplares: 
-
-       db.session.execute(
-         text("INSERT INTO emprestimo (id_exemplar, id_usuario ,data_inicio) VALUES ({},{},'{}')".format(exemplar, id, data))
-      )
-      
-    db.session.commit()
-
-      
-
-
   return redirect(url_for("home"))
-    
+
+
+
+
 
 
 @app.route("/perfil", methods = ["GET", "POST"])
 @login_required
 def profile(): 
 
-  if request.method == "GET": 
-    query = text(r'SELECT * from emprestimo WHERE id_usuario = :val')
-    resultado = db.session.execute(query, {"val": current_user.get_id()}).fetchall()
-
-    return render_template("perfil.html", resultado = resultado)
+  return render_template("perfil.html", resultado = None)
 
 
 
@@ -102,21 +93,28 @@ def login():
     senha = request.form["senha"]
     tipo_usuario = request.form["tipo_usuario"]
 
-    print(tipo_usuario)
 
     user = None
-    if tipo_usuario == "1":
-      user =  db.session.query(Bibliotecario).filter_by(cpf = cpf, senha = senha).first()
-      session['table'] = 1
 
-    elif tipo_usuario == "3": 
-      user =  db.session.query(Usuario).filter_by(cpf = cpf, senha = senha).first()
-      session['table'] = 2
+    #Usuario Comum
+    if tipo_usuario == "3":  
+      user =  mongo.db.usuario.find_one({"$and":[{"CPF": cpf}, {"senha":  senha}]})
+      session['user_type'] = 3
       
 
+    #Assistente
+    elif tipo_usuario == "2": 
+      user = mongo.db.assistente.find_one({"$and":[{"CPF": cpf}, {"senha":  senha}]})
+      session['user_type'] = 2
+
+    #Bibliotecario
+    else:
+      user = mongo.db.bibliotecario.find_one({"$and":[{"CPF": cpf}, {"senha":  senha}]})     
+      session['user_type'] = 1
+
     if user: 
-        login_user(user)
-        return redirect(url_for("home"))
+      login_user(User(user))
+      return redirect(url_for("home"))
     
     return redirect(url_for("login"))
     
@@ -130,41 +128,55 @@ def logout():
     
 
 @app.route("/register", methods=["GET", "POST"])
-def register(): 
+def register():   
+  error = None
+  if request.method == "POST":
+    data = request.form.to_dict()
+    user_type = data.pop('tipo')
 
-  if request.method == "POST": 
-    nome = request.form.get("nome")
-    cpf = request.form.get("cpf")
-    endereco = request.form.get("endereco")
-    telefone = request.form.get ("telefone")
-    tipo_usuario = request.form.get("tipo")
-    password = request.form.get("senha")
-    if tipo_usuario == "1": 
+    if user_type == '3': 
+      
+      someone = mongo.db.usuario.find_one({"CPF": data['cpf']})
 
-      bib = Bibliotecario(cpf = cpf, nome = nome, senha = password, endereco = endereco, telefone = telefone)
-      db.session.add(bib)
-      db.session.commit()
-
-      session['table'] = 1
-      login_user(bib)
-
-      return redirect(url_for("home"))
-    
-    elif tipo_usuario == "2": 
-      user = Usuario(cpf = cpf, 
-                     nome = nome, 
-                     senha = password, 
-                     endereco = endereco,
-                     telefone = telefone) 
-      db.session.add(user)
-      db.session.commit()
-
-      session['table'] = 2
-      login_user(user)
-
-      return redirect(url_for("home"))
+      if not someone: 
+        mongo.db.usuario.insert_one(data)
+        session['user_type'] = 3
+        login_user(User(data))
+        return redirect(url_for("home"))
+      else: 
+        error = "Usuario já existente!! CPF já existe"
 
       
-  return render_template("register.html")
+
+    elif user_type == '2': 
+        
+      someone = mongo.db.assistente.find_one({"CPF": data['cpf']})
+
+      if not someone: 
+        mongo.db.assistente.insert_one(data)
+        session['user_type'] = 2
+        login_user(User(data))
+        return redirect(url_for("home"))
+      else: 
+        error = "Usuario já existente!! CPF já existe"
+        
+
+    
+    else:
+      someone =  mongo.db.bibliotecario.find_one({"CPF": data['cpf']})
+    
+      if not someone: 
+        mongo.db.bibliotecario.insert_one(data)
+        session['user_type'] = 1
+        login_user(User(data))
+        return redirect(url_for("home"))
+      
+      else: 
+        error = "Usuario já existente!! CPF já existe"
+
+  return render_template("register.html", erro = error)
+
+
+
 
 app.run(debug=True)
